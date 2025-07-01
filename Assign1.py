d033a25d02f6fab52e229addb3d0df153049482b
sureshDGCP <sureshdasarwargcp@gmail.com>
2025-07-01 23:37:18 +0530

    Variables and datatypes and assign1

---
+++ Assign1.py
@@ -0,0 +1,6 @@
+## Hint
+x = input("enter your value")
+print(x)
+
+## ask the user to enter the values or variable values then it should give it datatype
+
---
+++ variables_datatypes.py
@@ -0,0 +1,51 @@
+print("Hello Suresh")
+
+''' print(5+"2")
+print("test")
+'''
+
+##print("5"+2)
+
+print("5+2")
+print("5"+"2")
+c= 7
+print("The result is "+ str(7))
+# same datatypes can be concatenated
+
+'''==================== variables ======= '''
+## valid variable examples
+name = "suresh"
+NAME = "SREEJA"
+Name = "SHRI ram"
+_name = "_dummy_name"
+name123 = 123
+
+print(str(name123)+NAME,name,Name,_name)
+
+## invalid variables
+
+##name@ = "test1"
+#123name = "test2"
+##t1 t2 = "test3"
+
+##try = "test4"
+##print(try)
+
+var1 = 5+2  ##(+ is operator and 5,2 is operand)
+
+'''=============Data types ============='''
+
+var2 = "cricket"
+
+print(type(var2)) ## type is keyword to check the datatype of variable   (<class 'str'>)
+
+var3 = -3
+var4 = 55
+print(type(var3),type(var4))   ##<class 'int'> <class 'int'>
+
+var5 = 23.56
+print(type(var5))   ## <class 'float'>
+
+var6 = True
+var7 = False
+print(type(var6),type(var7))   ## <class 'bool'> <class 'bool'>
